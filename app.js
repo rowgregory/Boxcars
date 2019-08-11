f5f9c24bf527642925f4979754fdd1acfedf5351
rowgregory <rowgregory@gmail.com>
2019-08-10 22:08:49 -0400

    changed file paths

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ document.querySelector(".btn-roll").addEventListener("click", () => {
     let diceImg = document.querySelector(".dice");
     let diceImg2 = document.querySelector(".dice2");
     diceImg.style.display = "block";
-    diceImg.src = `/images/dice-${dice}.png`;
+    diceImg.src = `./images/dice-${dice}.png`;
 
     if (dice === 6 && lastDice === 6) {
       // Player looses score
@@ -19,7 +19,7 @@ document.querySelector(".btn-roll").addEventListener("click", () => {
       document.querySelector(`#score-${activePlayer}`).classList.add("scale");
       diceImg.src = `dice-${dice}.png`;
       diceImg.style.left = "40%";
-      diceImg2.src = `/images/dice-${dice}.png`;
+      diceImg2.src = `./images/dice-${dice}.png`;
       diceImg2.style.left = "52%";
       diceImg2.style.display = "block";
       setTimeout(() => {
